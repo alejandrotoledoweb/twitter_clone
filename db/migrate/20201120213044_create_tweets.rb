e79cebfe3041e807b851ca4bd86eb31add2cae57
alejandrotoledoweb <atoledofr@gmail.com>
2020-11-20 16:48:26 -0500

    add twitter model and controller

---
+++ db/migrate/20201120213044_create_tweets.rb
@@ -0,0 +1,10 @@
+# this is the tweet class
+class CreateTweets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweets do |t|
+      t.text :tweet
+
+      t.timestamps
+    end
+  end
+end
