d7a72b61afc675b2481f9a91dafae907caa4707e
alejandrotoledoweb <atoledofr@gmail.com>
2020-11-23 19:46:10 -0500

    add registration syntax missing and test with registration info

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
   before_action :authentication_user!, except: [:index, :show]
 
-  
+
   # GET /tweets
   # GET /tweets.json
   def index
@@ -17,7 +17,7 @@ class TweetsController < ApplicationController
 
   # GET /tweets/new
   def new
-    @tweet = Tweet.new
+    @tweet = current_user.tweets.build
   end
 
   # GET /tweets/1/edit
@@ -27,7 +27,7 @@ class TweetsController < ApplicationController
   # POST /tweets
   # POST /tweets.json
   def create
-    @tweet = Tweet.new(tweet_params)
+    @tweet = current_user.tweets.build(tweet_params)
 
     respond_to do |format|
       if @tweet.save
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_220203) do
+ActiveRecord::Schema.define(version: 2020_11_24_002336) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
